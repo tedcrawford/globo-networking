7afe49c9ce28d1ea64c85ae561a7cbb34bccbc4e
admin.tcrawford <ted.crawford@vocus.com.au>
2026-03-31 15:28:16 +1100

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,53 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+/*
+
+
+import {
+  to = module.main.aws_vpc.this[0]
+  id = "vpc-0ca30e0b5030addd2" #VPC
+}
+
+import {
+  to = module.main.aws_subnet.public[0]
+  id = "subnet-0f05b16f0ecd3ebb1" #PublicSubnet1
+}
+
+import {
+  to = module.main.aws_subnet.public[1]
+  id = "subnet-0630b2ddefccf2b8b" #PublicSubnet2
+}
+
+import {
+  to = module.main.aws_internet_gateway.this[0]
+  id = "igw-0712fe5174d2cc128" #InternetGateway
+}
+
+import {
+  to = module.main.aws_route.public_internet_gateway[0]
+  id = "rtb-0cdad18bd0d906555_0.0.0.0/0" #DefaultPublicRoute
+}
+
+import {
+  to = module.main.aws_route_table.public[0]
+  id = "rtb-0cdad18bd0d906555" #PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[0]
+  id = "subnet-0f05b16f0ecd3ebb1/rtb-0cdad18bd0d906555" #PublicSubnet1/PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-0630b2ddefccf2b8b/rtb-0cdad18bd0d906555" #PublicSubnet2/PublicRouteTable
+}
+
+import {
+  to = aws_security_group.ingress
+  id = "sg-058e1f57491a71780" #NoIngressSecurityGroup
+}
+
+
+*/
